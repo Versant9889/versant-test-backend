30b1e6396aefc9876187d7a2990979cc6b480146
Shubham <your-sds1369@gmail.com>
2025-04-26 14:43:53 +0530

    Update questions.js route to /questions for clarity

--- routes/questions.js
+++ routes/questions.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.get('/questions', (req, res) => {
   res.send('Questions API is working');
 });
 
